da255104ca6fd2eb4a58da37b5e504d688260a34
Kanha-Codes-XD <kanhakhantaal@gmail.com>
2025-09-26 22:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from src.embedder.build_embeddings import build_embeddings
 from src.classifier.train_classifier import train_hybrid_model
 from src.logic import ChatLogic
-import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")   
 
@@ -17,21 +16,12 @@ def main():
         print("Training hybrid classifier...")
         train_hybrid_model()
 
-
     print("Starting IT Support Chatbot.........")
 
 
     bot = ChatLogic()
-    # bot.run()
     bot.run()
 
-    # response = bot.jira_create_issue(initial_query, description)
-    # if response.status_code == 201:
-    #     print("\nIssue created successfully.")
-    #     print("\nResponse:", response.text)
-        
-    # else:
-    #     print("\nFailed to create issue.")
     
 
 if __name__ == "__main__":
